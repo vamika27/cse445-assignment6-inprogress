1324cf212d50292384f0188ab99314c293be558e
brandonhbse <78253023+brandonhbse@users.noreply.github.com>
2026-04-29 22:12:18 -0700

    Add files via upload

---
+++ CryptoUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CryptoUtil
+{
+    public class CryptoUtils // GeeksForGeeks
+    {
+        public static string hashMe(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
+
